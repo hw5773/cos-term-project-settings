a57ac2c32ed5fe0cb21cb16c8b6fbef2d941a903
Hyunwoo Lee <hw5773@gmail.com>
2023-04-24 23:41:02 +0900

    add the script

---
+++ iptables.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
+apt-get install iptables-persistent
